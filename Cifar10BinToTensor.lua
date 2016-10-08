d7a28455307b2cdd17a02bb870d372fabaf07af2
Mark Buckler <markabuckler@gmail.com>
2016-10-08 12:30:10 -0400

    Modifying the converter to work with our pipeline

--- Cifar10BinToTensor.lua
+++ Cifar10BinToTensor.lua
@@ -1,7 +1,7 @@
 require 'torch'
 
-os.execute('wget -c http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz')
-os.execute('tar -xvf cifar-10-binary.tar.gz')
+--os.execute('wget -c http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz')
+--os.execute('tar -xvf cifar-10-binary.tar.gz')
 local function convertCifar10BinToTorchTensor(inputFnames, outputFname)
    local nSamples = 0
    for i=1,#inputFnames do
@@ -15,7 +15,7 @@ local function convertCifar10BinToTorchTensor(inputFnames, outputFname)
       m:close()
    end
 
-   local label = torch.ByteTensor(nSamples)
+   local labels = torch.ByteTensor(nSamples)
    local data = torch.ByteTensor(nSamples, 3, 32, 32)
 
    local index = 1
@@ -27,7 +27,7 @@ local function convertCifar10BinToTorchTensor(inputFnames, outputFname)
       local nSamplesF = length / 3073 -- 1 label byte, 3072 pixel bytes
       m:seek(1)
       for j=1,nSamplesF do
-         label[index] = m:readByte()
+         labels[index] = m:readByte()
          local store = m:readByte(3072)
          data[index]:copy(torch.ByteTensor(store))
          index = index + 1
@@ -35,19 +35,23 @@ local function convertCifar10BinToTorchTensor(inputFnames, outputFname)
       m:close()
    end
 
+   -- Add 1 to labels so as to be compatible with CrossEntropyCriterion
+   labels:add(1)
+
    local out = {}
    out.data = data
-   out.label = label
+   out.labels = labels
    print(out)
    torch.save(outputFname, out)
 end
 
-convertCifar10BinToTorchTensor({'cifar-10-batches-bin/data_batch_1.bin',
-                                'cifar-10-batches-bin/data_batch_2.bin',
-                                'cifar-10-batches-bin/data_batch_3.bin',
-                                'cifar-10-batches-bin/data_batch_4.bin',
-                                'cifar-10-batches-bin/data_batch_5.bin'},
-   'cifar10-train.t7')
+convertCifar10BinToTorchTensor({
+	'/root/datasets/cifar-10/v0/data_batch_1.bin',
+	'/root/datasets/cifar-10/v0/data_batch_2.bin',
+	'/root/datasets/cifar-10/v0/data_batch_3.bin',
+	'/root/datasets/cifar-10/v0/data_batch_4.bin',
+	'/root/datasets/cifar-10/v0/data_batch_5.bin'},
+					'cifar10-train.t7')
 
-convertCifar10BinToTorchTensor({'cifar-10-batches-bin/test_batch.bin'},
+convertCifar10BinToTorchTensor({'/root/datasets/cifar-10/v0/test_batch.bin'},
    'cifar10-test.t7')
